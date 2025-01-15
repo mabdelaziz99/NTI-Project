895f5dd0d7d8991bebeb2a71ba49c650183c2aa8
Mahmoud <mabdelaziz25899@gmail.com>
2025-01-15 14:17:01 +0200

    added UserId to addToCart function

--- websie/src/app/products-list/products-list.component.ts
+++ websie/src/app/products-list/products-list.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../services/products.service';
+import {AuthService} from '../services/auth.service';
 
 @Component({
   selector: 'app-products-list',
   standalone: false,
   
   templateUrl: './products-list.component.html',
-  styleUrl: './products-list.component.css'
+  styleUrl: './products-list.component.css',
 })
 export class ProductsListComponent implements OnInit {
   products!:any[];
  staticURL=''
-constructor(private _productS:ProductsService){
+constructor(private _productS:ProductsService, private _auth: AuthService){
 }
   ngOnInit(): void {
     this.staticURL = this._productS.staticFilesURL;
@@ -20,9 +21,11 @@ constructor(private _productS:ProductsService){
     console.log(data)
    })
   }
+
   addToCart(id: string){
     console.log(id)
-    console.log('kkkkk')
+    
+    console.log(this._auth.getDecodeToken().userId)
   }
   
   
